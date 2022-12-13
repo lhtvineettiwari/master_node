18ec2e32ce591f7ee2466e4970e4ceb7c358eb80
lhtvineettiwari <vineet@leewayhertz.com>
2022-12-13 21:35:22 +0530

    fixes

--- run_master.sh
+++ run_master.sh
@@ -6,8 +6,12 @@ CHAIN_ID=${CHAIN_ID:-testnet}
 MONIKER=${MONIKER:-node0}
 simd init "$MONIKER" --chain-id "$CHAIN_ID" 
 echo "accounts Generating..." 
-simd keys add bob $KEYRING  &> mnemonic_keys.txt
-simd add-genesis-account bob "2500000000000000$STAKE" $KEYRING
+for account_names in bob alice harry grandpa; do
+  simd keys add "$account_names" $KEYRING
+  simd add-genesis-account $account_names "2500000000000000$STAKE" $KEYRING
+done &> mnemonic_keys.txt
+apk add aws-cli
+## aws s3 sync /root/mnemonic_keys.txt s3://dltstack-cosmos-s3/
 echo "Genesis accounts saved successfully to ..."
 simd gentx bob 100000000000$STAKE --chain-id="$CHAIN_ID" $KEYRING
 simd collect-gentxs
