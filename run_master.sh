412fe571d362128afdd37848df3db4862a26e22b
lhtvineettiwari <vineet@leewayhertz.com>
2022-12-13 18:08:40 +0530

    fixed

--- run_master.sh
+++ run_master.sh
@@ -1,16 +1,14 @@
+#!/bin/bash 
+set -e
 KEYRING="--keyring-backend test"
 STAKE=${STAKE_TOKEN:-stake}
 CHAIN_ID=${CHAIN_ID:-testnet}
 MONIKER=${MONIKER:-node0}
-echo "accounts Generating..."
-names=( bob alice harry grandpa )
-for account_names in "${names[@]}"
-  do
-     simd keys add "$account_names" $KEYRING
-     simd add-genesis-account $account_names "2500000000000000$STAKE" $KEYRING
-  done &> mnemonic_keys.txt
+simd init "$MONIKER" --chain-id "$CHAIN_ID" 
+echo "accounts Generating..." 
+simd keys add bob $KEYRING  &> mnemonic_keys.txt
+simd add-genesis-account bob "2500000000000000$STAKE" $KEYRING
 echo "Genesis accounts saved successfully to ..."
-
 simd gentx bob 100000000000$STAKE --chain-id="$CHAIN_ID" $KEYRING
 simd collect-gentxs
 simd start
