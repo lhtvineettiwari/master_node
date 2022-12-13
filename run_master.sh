74fdb9fa1ef6c9fae5fd941c287f703feff0d63a
lhtvineettiwari <vineet@leewayhertz.com>
2022-12-13 13:37:15 +0530

    master node

---
+++ run_master.sh
@@ -0,0 +1,16 @@
+KEYRING="--keyring-backend test"
+STAKE=${STAKE_TOKEN:-stake}
+CHAIN_ID=${CHAIN_ID:-testnet}
+MONIKER=${MONIKER:-node0}
+echo "accounts Generating..."
+names=( bob alice harry grandpa)
+for account_names in "${names[@]}"
+  do
+     simd keys add "$account_names" $KEYRING
+     simd add-genesis-account $account_names "2500000000000000$STAKE" $KEYRING
+  done &> mnemonic_keys.txt
+echo "Genesis accounts saved successfully to ..."
+
+simd gentx bob 100000000000$STAKE --chain-id="$CHAIN_ID" $KEYRING
+simd collect-gentxs
+simd start
