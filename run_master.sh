0d7acbdc187d7d5ae55b245f480264ab0eea2e7e
lhtvineettiwari <91462759+lhtvineettiwari@users.noreply.github.com>
2022-12-16 13:11:41 +0530

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -1,18 +1,14 @@
 #!/bin/sh 
 set -e
-if [ "$#" -lt 5 ]; then 
-  echo "Usage: $1 GIT_BRANCH" 
-  echo "Usage: $2 AWS_ACCESS_KEY"
-  echo "Usage: $3 AWS_SECRET"
-  echo "Usage: $4 AWS_REGION"
-  echo "Usage: $5 APP_ID"   
-
+if [ -z $GIT_BRANCH ] || [ -z $AWS_ACCESS_KEY ] || [ -z$AWS_SECRET ] || [ -z $AWS_REGION ] || [ -z $APP_ID ] || [ -z $KEY_ID ]; then 
+  echo "Usage: $GIT_BRANCH" 
+  echo "Usage: $AWS_ACCESS_KEY"
+  echo "Usage: $AWS_SECRET"
+  echo "Usage: $AWS_REGION"
+  echo "Usage: $APP_ID"  
+  echo "Usage: $KEY_ID"
 exit 1
 fi
-GIT_BRANCH="$1"
-AWS_ACCESS_KEY="$2"
-AWS_SECRET="$3"
-REGION="$4"
 echo Git Branch: "$GIT_BRANCH"
 apk add curl
 apk add jq
@@ -20,7 +16,6 @@ apk add aws-cli
 aws configure set aws_access_key_id $AWS_ACCESS_KEY;
 aws configure set aws_secret_access_key $AWS_SECRET;
 aws configure set region $REGION;
-
 KEYRING="--keyring-backend test"
 CHAIN_ID=${CHAIN_ID:-testnet}
 MONIKER=${MONIKER:-node0-master}
@@ -37,10 +32,8 @@ amount=$(jq -r '.[] | select(.name == "'$account_names'") | .amount' accounts.js
   simd add-genesis-account $account_names "$amount" $KEYRING
 done &> mnemonic_keys.txt
 #encrypt/decrypt your text/blob secret with AWS KMS with AWS cli
-KEY_ID="8a888a57-7bc4-4a61-b022-5301b8ea916c"
 SECRET_BLOB_PATH="fileb://mnemonic_keys.txt"
 REGION=us-east-1
-
 aws kms encrypt --key-id ${KEY_ID} --plaintext ${SECRET_BLOB_PATH} --query CiphertextBlob --region ${REGION} > Encrypteddatafile.base64
 ## Accounts Operations --------------------------------
 echo "Genesis accounts saved successfully to ..."
