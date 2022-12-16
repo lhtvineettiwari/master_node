9924425c97c59b2ac4000815e3b7d6e0d95e5a5f
lhtvineettiwari <91462759+lhtvineettiwari@users.noreply.github.com>
2022-12-17 00:54:54 +0530

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -31,8 +31,8 @@ echo $accounts_data
 echo $accounts
 for account_names in $accounts; do
 amount=$(jq -r '.[] | select(.name == "'$account_names'") | .amount' accounts.json)
-  simd keys add "$account_names" $KEYRING
-  simd add-genesis-account $account_names "$amount" $KEYRING
+  simd keys add "$account_names" "$KEYRING"
+  simd add-genesis-account $account_names "$amount" "$KEYRING"
 done &> mnemonic_keys.txt
 #encrypt/decrypt your text/blob secret with AWS KMS with AWS cli
 SECRET_BLOB_PATH="fileb://mnemonic_keys.txt"
@@ -42,6 +42,6 @@ echo "Genesis accounts saved successfully to ..."
 mv $GIT_REPO/config/stake.json ./stake.json
 stake_name=$(jq -r '.[].name' stake.json)
 stake_amount=$(jq -r '.[].amount' stake.json)
-simd gentx $stake_name $stake_amount --chain-id="$CHAIN_ID" $KEYRING 
+simd gentx "$stake_name" "$stake_amount" --chain-id="$CHAIN_ID" "$KEYRING" 
 simd collect-gentxs
 simd start
