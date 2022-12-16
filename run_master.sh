92c7caee3760b0312a13260be026a845c7b4f0c4
lhtvineettiwari <91462759+lhtvineettiwari@users.noreply.github.com>
2022-12-16 17:44:25 +0530

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -18,12 +18,14 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY;
 aws configure set aws_secret_access_key $AWS_SECRET;
 aws configure set region $AWS_REGION;
 KEYRING="--keyring-backend test"
-CHAIN_ID=${CHAIN_ID:-testnet}
-simd init "$MONIKER" --chain-id "$CHAIN_ID" 
+# CHAIN_ID=${CHAIN_ID:-testnet}
 git clone https://$GIT_USERNAME:$GIT_APP_PASS@bitbucket.org/leewayhertz/$GIT_REPO.git -b $GIT_BRANCH 
-mv $GIT_REPO/config/genesis.json /root/.simapp/config/genesis.json
+cp $GIT_REPO/config/genesis.json /root/.simapp/config/genesis.json
+CHAIN_ID=$(jq -r '.chain_id' $GIT_REPO/config/genesis.json)
+simd config chain-id $CHAIN_ID
+simd init "$MONIKER" --chain-id $CHAIN_ID 
 # Accounts Generating and adding in to the genesis
-mv $GIT_REPO/config/accounts.json  ./accounts.json
+cp $GIT_REPO/config/accounts.json  ./accounts.json
 accounts=$(jq -r '.[].name' accounts.json)
 echo $accounts_data
 echo $accounts
@@ -37,7 +39,7 @@ SECRET_BLOB_PATH="fileb://mnemonic_keys.txt"
 aws kms encrypt --key-id ${KEY_ID} --plaintext ${SECRET_BLOB_PATH} --query CiphertextBlob --region ${AWS_REGION} > Encrypteddatafile.base64
 ## Accounts Operations --------------------------------
 echo "Genesis accounts saved successfully to ..."
-mv $GIT_REPO/config/stake.json ./stake.json
+cp $GIT_REPO/config/stake.json ./stake.json
 stake_name=$(jq -r '.[].name' stake.json)
 stake_amount=$(jq -r '.[].amount' stake.json)
 simd gentx $stake_name $stake_amount --chain-id="$CHAIN_ID" $KEYRING 
