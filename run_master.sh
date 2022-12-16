55f61d382868149f460cd4c21ec64bd3ee2273dc
lhtvineettiwari <91462759+lhtvineettiwari@users.noreply.github.com>
2022-12-16 13:13:31 +0530

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -1,12 +1,13 @@
 #!/bin/sh 
 set -e
-if [ -z $GIT_BRANCH ] || [ -z $AWS_ACCESS_KEY ] || [ -z$AWS_SECRET ] || [ -z $AWS_REGION ] || [ -z $APP_ID ] || [ -z $KEY_ID ]; then 
+if [ -z $GIT_BRANCH ] || [ -z $AWS_ACCESS_KEY ] || [ -z$AWS_SECRET ] || [ -z $AWS_REGION ] || [ -z $APP_ID ] || [ -z $KEY_ID ]|| [ -z $MONIKER ]; then 
   echo "Usage: $GIT_BRANCH" 
   echo "Usage: $AWS_ACCESS_KEY"
   echo "Usage: $AWS_SECRET"
   echo "Usage: $AWS_REGION"
   echo "Usage: $APP_ID"  
   echo "Usage: $KEY_ID"
+  echo "Usage: $MONIKER"
 exit 1
 fi
 echo Git Branch: "$GIT_BRANCH"
@@ -18,7 +19,6 @@ aws configure set aws_secret_access_key $AWS_SECRET;
 aws configure set region $REGION;
 KEYRING="--keyring-backend test"
 CHAIN_ID=${CHAIN_ID:-testnet}
-MONIKER=${MONIKER:-node0-master}
 simd init "$MONIKER" --chain-id "$CHAIN_ID" 
 curl https://raw.githubusercontent.com/lhtvineettiwari/genesis/$GIT_BRANCH/genesis.json  >> ~/.simapp/genesis.json
 # Accounts Generating and adding in to the genesis
